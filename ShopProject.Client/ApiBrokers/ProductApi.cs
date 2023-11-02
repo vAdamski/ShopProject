1f68c8b22965b8347fac9c5ae0808c9181df28f8
Adam Ludwiczak <adam.ludwiczak@keikostudio.com>
2023-11-02 23:48:01 +0100

    Update ApiBroker for products

--- ShopProject.Client/ApiBrokers/ProductApi.cs
+++ ShopProject.Client/ApiBrokers/ProductApi.cs
@@ -1,4 +1,5 @@
 using ShopProject.Shared.Dtos;
+using ShopProject.Shared.ViewModels;
 
 namespace ShopProject.Client.ApiBrokers;
 
@@ -7,17 +8,18 @@ public class ProductApi : ApiBroker, IProductApi
     public ProductApi(HttpClient httpClient) : base(httpClient)
     {
     }
-    
+
     private const string ProductRelativeUrl = "https://localhost:6001/api/products";
 
-    public async Task CreateProduct(MultipartFormDataContent multipartFormDataContent)
-    {
+    public async Task CreateProduct(MultipartFormDataContent multipartFormDataContent) =>
         await PostAsync<MultipartFormDataContent>($"{ProductRelativeUrl}/create-product", multipartFormDataContent);
-    }
     
+    public async Task<EditableProductsListViewModel> GetEditableProducts() =>
+        await GetFromJsonAsync<EditableProductsListViewModel>($"{ProductRelativeUrl}/get-editable-products");
 }
 
 public interface IProductApi
 {
     Task CreateProduct(MultipartFormDataContent multipartFormDataContent);
+    Task<EditableProductsListViewModel> GetEditableProducts();
 }
