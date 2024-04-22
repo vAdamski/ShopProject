bb47b89b044e73bd169b4b6dd35e71dfad8ce173
Adam Ludwiczak <64928495+vAdamski@users.noreply.github.com>
2024-04-22 10:12:14 +0200

    Working authorization on Auth0

--- ShopProjectMauiBlazorApp/MauiProgram.cs
+++ ShopProjectMauiBlazorApp/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.Logging;
+using ShopProjectMauiBlazorApp.Auth0;
 
 namespace ShopProjectMauiBlazorApp;
 
@@ -12,6 +14,16 @@ public static class MauiProgram
             .ConfigureFonts(fonts => { fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"); });
 
         builder.Services.AddMauiBlazorWebView();
+        
+        builder.Services.AddSingleton(new Auth0Client(new()
+        {
+            Domain = "dev-u8i8ssxpwy2t06gm.us.auth0.com",
+            ClientId = "Vxa9egkZGTQzdvYhmxL5Bxhjp9QbUPlk",
+            Scope = "openid",
+            RedirectUri = "myapp://callback",
+        }));
+        builder.Services.AddAuthorizationCore();
+        builder.Services.AddScoped<AuthenticationStateProvider, Auth0AuthenticationStateProvider>();
 
 #if DEBUG
         builder.Services.AddBlazorWebViewDeveloperTools();
